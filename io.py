4be48564deb1f5443bde42000b67b1ed4449cc5f
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2018-03-01 19:22:03 +0100

    finish input reading and fix utils

--- io.py
+++ io.py
@@ -1,20 +1,26 @@
 import sys
 from utils import *
 
-def read_data(fpath):
+def read_data(in_data):
+    examples = {
+            'a' : 'a_example.in',
+            'b' : 'b_should_be_easy.in',
+            'c' : 'c_no_hurry.in',
+            'd' : 'd_metropolis.in',
+            'e' : 'e_high_bonus.in'
+    }
+    fpath = "./inputs/" + examples[in_data]
+
     f = open(fpath, 'r')
     data = f.readlines()
 
     R, C, F, N, B, T = data[0].split()
 
     rides = []
-    for line in data[1:]:
+    for i, line in enumerate(data[1:]):
         a, b, x, y, s, f = line.split()
         sp = Pos(a, b)
         ep = Pos(x, y)
-        print(sp, ep, s, f)
-        rides.append(Ride(s, f, sp, ep))
+        rides.append(Ride(i, s, f, sp, ep))
 
     return R, C, F, N, B, T, rides
-
-read_data("./inputs/a_example.in")
