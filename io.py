9b4c105b4063ff6d3ee68f3b014d511331613995
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2018-03-01 19:10:28 +0100

    io skeleton more

--- io.py
+++ io.py
@@ -2,4 +2,19 @@ import sys
 from utils import *
 
 def read_data(fpath):
-    pass
+    f = open(fpath, 'r')
+    data = f.readlines()
+
+    R, C, F, N, B, T = data[0].split()
+
+    rides = []
+    for line in data[1:]:
+        a, b, x, y, s, f = line.split()
+        sp = Pos(a, b)
+        ep = Pos(x, y)
+        print(sp, ep, s, f)
+        rides.append(Ride(s, f, sp, ep))
+
+    return R, C, F, N, B, T, rides
+
+read_data("./inputs/a_example.in")
